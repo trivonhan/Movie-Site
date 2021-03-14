75c45b0bbb0b97015ac1fcbc69bd3fb9e1479c39
Huỳnh Minh Trí <trivonhan@gmail.com>
2021-03-14 22:48:54 +0700

    Update main

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
