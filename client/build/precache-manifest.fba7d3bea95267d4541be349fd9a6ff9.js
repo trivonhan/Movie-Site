290c3f777c150f33a928a2bdbae899f22599ccca
Huỳnh Minh Trí <trivonhan@gmail.com>
2021-03-15 03:15:55 +0700

    3h15

---
+++ client/build/precache-manifest.fba7d3bea95267d4541be349fd9a6ff9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed50488acb7eacb4b61b170a8658ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41be6c252336b599c84f",
+    "url": "/static/css/2.20d8cf1d.chunk.css"
+  },
+  {
+    "revision": "2944d780d88fecd57cae",
+    "url": "/static/css/main.843f47d5.chunk.css"
+  },
+  {
+    "revision": "41be6c252336b599c84f",
+    "url": "/static/js/2.145174ad.chunk.js"
+  },
+  {
+    "revision": "cb9dba4f10de6d683bdbb6d18dd954ff",
+    "url": "/static/js/2.145174ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2944d780d88fecd57cae",
+    "url": "/static/js/main.3b60a693.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0e4c6b31feadee72442de6770cb9ed67",
+    "url": "/static/media/Logo.0e4c6b31.png"
+  }
+]);
