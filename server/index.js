bf8346a704f843da56370792f535088f07e65dbc
Huỳnh Minh Trí <trivonhan@gmail.com>
2021-03-15 08:30:54 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,10 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    try{
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    }
+    catch (err) {console.log(error)}
   });
 }
 
